cc9428469e648289948c6c643117f7251b6dad65
parbez <imranbarbhuiya.fsd@gmail.com>
2022-09-26 23:43:55 +0530

    fix: remove $

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -14,10 +14,10 @@ export const BlockQuoteRegex = /^( *>>> ([\s\S]*))|^( *> [^\n]*(\n *> [^\n]*)*\n
 
 export const CodeBlockRegex = /^```(([a-z0-9-]+?)\n+)?\n*([^]+?)\n*```/i;
 
-export const EmoticonRegex = /^(¯\\_\(ツ\)_\/¯)$/;
+export const EmoticonRegex = /^(¯\\_\(ツ\)_\/¯)/;
 
-export const SpoilerRegex = /^\|\|([\s\S]+?)\|\|$/;
+export const SpoilerRegex = /^\|\|([\s\S]+?)\|\|/;
 
-export const StrikeThroughRegex = /^~~([\s\S]+?)~~(?!_)$/;
+export const StrikeThroughRegex = /^~~([\s\S]+?)~~(?!_)/;
 
 export const TextRegex = /^[\s\S]+?(?=[^0-9A-Za-z\s]|\n\n|\n|\w+:\S|$)/;
